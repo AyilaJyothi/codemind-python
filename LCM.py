0bbbfba16562009f9e85bb5b13bc8804118eecc1
AyilaJyothi <119840130+AyilaJyothi@users.noreply.github.com>
2023-05-21 00:08:06 +0530

    n,m=map(int,input().split()) if(n>m):     t=n else:     t=m i=1 while (i!=0):     if((i*t)%m==0 and (i*t)%n==0):         print(i*t)         break     i+=1

--- LCM.py
+++ LCM.py
@@ -1,4 +1,11 @@
-import math
-a,b=map(int,input().split())
-m=math.lcm(a,b)
-print(m)
+n,m=map(int,input().split())
+if(n>m):
+    t=n
+else:
+    t=m
+i=1
+while (i!=0):
+    if((i*t)%m==0 and (i*t)%n==0):
+        print(i*t)
+        break
+    i+=1
