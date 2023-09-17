1d5cae843f93db149410439f166f0eb6d3fb230f
AyilaJyothi <119840130+AyilaJyothi@users.noreply.github.com>
2023-09-17 21:34:37 +0530

    a,b,c,d,e=map(int,input().split()) x=(a+b+c+d+e)/5 if(x>=90):     print("Grade A") elif(x>=80 and x<90):     print("Grade B") elif(x>=70 and x<80):     print("Grade C") elif(x>=60 and x<70):     print("Grade D") elif(x>=50 and x<60):     print("Grade E") else:     print("Grade F")

---
+++ Grades.py
@@ -0,0 +1,14 @@
+a,b,c,d,e=map(int,input().split())
+x=(a+b+c+d+e)/5
+if(x>=90):
+    print("Grade A")
+elif(x>=80 and x<90):
+    print("Grade B")
+elif(x>=70 and x<80):
+    print("Grade C")
+elif(x>=60 and x<70):
+    print("Grade D")
+elif(x>=50 and x<60):
+    print("Grade E")
+else:
+    print("Grade F")
