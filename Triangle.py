50cc2dd3b0344b4b1cd4d31ddbb606f15f7a28bf
AyilaJyothi <119840130+AyilaJyothi@users.noreply.github.com>
2023-10-29 11:30:59 +0530

    a,b,c=map(int,input().split()) if(a==b and b==c and c==a):     print("Equilateral triangle") elif(a!=b and b!=c and c!=a):     print("Scalene triangle") else:     print("Isosceles triangle")

--- Triangle.py
+++ Triangle.py
@@ -1,7 +1,7 @@
 a,b,c=map(int,input().split())
-if(a==b and b==c and a==c):
+if(a==b and b==c and c==a):
     print("Equilateral triangle")
-elif(a==b or b==c or a==c):
-    print("Isosceles triangle")
-else:
+elif(a!=b and b!=c and c!=a):
     print("Scalene triangle")
+else:
+    print("Isosceles triangle")
