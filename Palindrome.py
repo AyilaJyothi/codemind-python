0a19fd6d0178e6c1f04ccffcd19be0739983ac3a
AyilaJyothi <119840130+AyilaJyothi@users.noreply.github.com>
2023-11-04 22:47:25 +0530

    a=input() if(a[::-1]==a):     print("Palindrome") else:     print("Not Palindrome")

--- Palindrome.py
+++ Palindrome.py
@@ -1,11 +1,5 @@
-n=int(input())
-t=n
-rev=0
-while(n!=0):
- d=n%10
- rev=rev*10+d
- n=n//10
-if(t==rev):
- print("True")
+a=input()
+if(a[::-1]==a):
+    print("Palindrome")
 else:
-    print('False')
+    print("Not Palindrome")
